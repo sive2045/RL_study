bb015168090a1d110318e230d080ce46941e3b13
sive2045 <lc9902130509@gmail.com>
2022-02-14 21:08:03 +0900

    init: gym install TBD install 설치하기 넘 빡세다

--- openai_spiiningup/gym_test.py
+++ openai_spiiningup/gym_test.py
@@ -1,11 +1,6 @@
 import gym
-
-env = gym.make('MountainCar-v0')
+env = gym.make('CartPole-v0')
 env.reset()
-step = 0
-scroe = 0
-
-
-
-while(True):
+for _ in range(1000):
     env.render()
+    env.step(env.action_space.sample())
