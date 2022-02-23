da457bb893f9316a9e56bcf0bfb8ac1ceedca735
sive2045 <lc9902130509@gmail.com>
2022-02-23 22:05:25 +0900

    feat: Ex 2_11 계산이 오래 걸려 코드부터 올림

--- RL2nd/Ch_2/examples.py
+++ RL2nd/Ch_2/examples.py
@@ -1,6 +1,11 @@
 """
+Examples
+
 Ex 2.5
 Non-stationary problem example
+
+Ex 2.11
+Non-stationary parameter study 
 """
 from Bandit import Bandit
 import matplotlib
@@ -103,7 +108,41 @@ def figure_ex_2_5(runs=2000, time=10_000):
     plt.savefig(current_path + '/images/figure_ex_2_5.png')
     plt.close()
 
+def figure_ex_2_11(runs=2000, time=10_000):
+    labels = ['epsilon-greedy', 'gradient bandit',
+              'UCB', 'optimistic initialization', 'constant step size greedy']
+    generators = [lambda epsilon: BanditNonStationary(epsilon=epsilon, sample_averages=True),
+                  lambda alpha: BanditNonStationary(gradient=True, step_size=alpha, gradient_baseline=True),
+                  lambda coef: BanditNonStationary(epsilon=0, UCB_param=coef, sample_averages=True),
+                  lambda initial: BanditNonStationary(epsilon=0, initial=initial, step_size=0.1),
+                  lambda exp: BanditNonStationary(epsilon=exp, exp_weighted=True)]
+    parameters = [np.arange(-7, -1, dtype=np.float),
+                  np.arange(-5, 2, dtype=np.float),
+                  np.arange(-4, 3, dtype=np.float),
+                  np.arange(-2, 3, dtype=np.float),
+                  np.arange(-7, -1, dtype=np.float)]
+    bandits = []
+    for generator, parameter in zip(generators, parameters):
+        for param in parameter:
+            bandits.append(generator(pow(2, param)))
+
+    _, average_rewards = simulate(runs, time, bandits)
+    rewards = np.mean(average_rewards, axis=1)
+
+    i = 0
+    for label, parameter in zip(labels, parameters):
+        l = len(parameter)
+        plt.plot(parameter, rewards[i:i+l], label=label)
+        i += l
+    plt.xlabel('Parameter($2^x$)')
+    plt.ylabel('Average reward')
+    plt.legend()
+
+    plt.savefig(current_path + '/images/figure_ex_2_11.png')
+    plt.close()
+
 if __name__ == '__main__':
     print("Start the processing....")
-    figure_ex_2_5()
+    #figure_ex_2_5()
+    figure_ex_2_11()
     print('Processing completed')
