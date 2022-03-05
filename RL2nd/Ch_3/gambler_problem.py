96e5a727fbe9033f7531e2adad2ffd0c244662e7
sive2045 <lc9902130509@gmail.com>
2022-03-05 18:48:32 +0900

    init gambelr's problem

---
+++ RL2nd/Ch_3/gambler_problem.py
@@ -0,0 +1,8 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.table import Table
+import os
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+matplotlib.use('Agg')
