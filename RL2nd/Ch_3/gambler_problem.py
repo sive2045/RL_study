c619733056e221525f793d662b4811704e93905b
sive2045 <lc9902130509@gmail.com>
2022-03-08 18:57:35 +0900

    fixed: gambler problem merge to ch. 4

--- RL2nd/Ch_3/gambler_problem.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.table import Table
-import os
-current_path = os.path.dirname(os.path.abspath(__file__))
-
-matplotlib.use('Agg')
