9aef19d9cf6181c6043a7750fcb90f0ec409abc1
sive2045 <lc9902130509@gmail.com>
2022-03-15 22:37:36 +0900

    feat: car rental

---
+++ RL2nd/Ch_4/car_rental_ps.py
@@ -0,0 +1,227 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import poisson
+import os
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# maximum # of cars in each loaction
+MAX_CARS = 20
+
+# maximum # of cars to move during night
+MAX_MOVE_OF_CARS = 5
+
+# expectation for rental request in first loaction
+RENTAL_EXPECT_REQUEST_FIRST_LOC = 3
+
+# expectation for rental request in second loaction
+RENTAL_EXPECT_REQUEST_SECOND_LOC = 3
+
+# expectation for # of cars retuned in first location
+RETURN_FIRST_LOC = 3
+
+# expectation for # of cars retuned in second location
+RETURN_SECOND_LOC = 2
+
+DISCOUNT = 0.9
+
+# credit earned by a car
+RENTAL_CREDIT = 10
+
+# cost of moving a car
+MOVE_CAR_COST = 2
+
+# all possible actions
+actions = np.arange(-MAX_MOVE_OF_CARS, MAX_MOVE_OF_CARS + 1)
+
+# An up bound for poission distribution
+POISSON_UPPER_BOUND = 11
+
+# Probability for poission distribution
+# @lam: lambda sould be less than 10 for this fucntion
+possion_cache = dict()
+
+def poisson_probability(n, lam):
+    global possion_cache
+    key = n * 10 + lam
+    if key not in possion_cache:
+        possion_cache[key] = poisson.pmf(n, lam)
+    return possion_cache[key]
+
+def expected_return(state, action, state_value, constant_returned_cars):
+    """
+    @state: [# of cars in first loc, # of cars in second loc]
+    @action: positvie, moving cars from first location to second location
+             negative, moving cars form second loaction to first location
+    @statevalue: state value matrix
+    @constant_returned_cars:  if set True, model is simplified such that
+    the # of cars returned in daytime becomes constant
+    rather than a random value from poisson distribution, which will reduce calculation time
+    and leave the optimal policy/value state matrix almost the same
+    """
+    # init total return
+    returns = 0.0
+
+    # cost for moving cars
+    returns -= MOVE_CAR_COST * abs(action)
+
+    # moving cars
+    NUM_OF_CARS_FIRST_LOC = min(state[0] - action, MAX_CARS)
+    NUM_OF_CARS_SECOND_LOC = min(state[1] + action, MAX_CARS)
+
+    # go throguh all possible rental requests
+    for rental_request_first_loc in range(POISSON_UPPER_BOUND):
+        for rental_request_second_loc in range(POISSON_UPPER_BOUND):
+            # probability for current combination of rental requests
+            prob = poisson_probability(rental_request_first_loc, RENTAL_EXPECT_REQUEST_FIRST_LOC) * \
+                poisson_probability(rental_request_second_loc, RENTAL_EXPECT_REQUEST_SECOND_LOC)
+            
+            num_of_cars_first_loc = NUM_OF_CARS_FIRST_LOC
+            num_of_cars_second_loc = NUM_OF_CARS_SECOND_LOC
+
+            # valid rental requests should be less than actual # of cars
+            valid_rental_first_loc = min(num_of_cars_first_loc, rental_request_first_loc)
+            valid_rental_second_loc = min(num_of_cars_second_loc, rental_request_second_loc)
+
+            # get credits for renting
+            reward = (valid_rental_first_loc + valid_rental_second_loc) * RENTAL_CREDIT
+            num_of_cars_first_loc -= valid_rental_first_loc
+            num_of_cars_second_loc -= valid_rental_second_loc
+            
+            if constant_returned_cars:
+                # get returned cars, those caras can be used for renting tomorrow
+                returned_cars_first_loc = RETURN_FIRST_LOC
+                returned_cars_second_loc = RETURN_SECOND_LOC
+                num_of_cars_first_loc = min(num_of_cars_first_loc + returned_cars_first_loc, MAX_CARS)
+                num_of_cars_second_loc = min(num_of_cars_second_loc + returned_cars_second_loc, MAX_CARS)
+                returns += prob*(reward + DISCOUNT*state_value[num_of_cars_first_loc, num_of_cars_second_loc])
+            else:
+                for returned_cars_first_loc in range(POISSON_UPPER_BOUND):
+                    for returned_cars_second_loc in range(POISSON_UPPER_BOUND):
+                        prob_return = poisson_probability(
+                            returned_cars_first_loc, RETURN_FIRST_LOC) * poisson_probability(returned_cars_second_loc, RETURN_SECOND_LOC)
+                        num_of_cars_first_loc_ = min(num_of_cars_first_loc + returned_cars_first_loc, MAX_CARS)
+                        num_of_cars_second_loc_ = min(num_of_cars_second_loc + returned_cars_second_loc, MAX_CARS)
+                        prob_ = prob_return * prob
+                        returns += prob_ * (reward + DISCOUNT *
+                                            state_value[num_of_cars_first_loc_, num_of_cars_second_loc_])
+    return returns
+
+def figure_4_2_1(constant_returned_cars=True):
+    value = np.zeros((MAX_CARS + 1, MAX_CARS + 1))
+    policy = np.zeros(value.shape, dtype=np.int)
+
+    iterations = 0
+    _, axes = plt.subplots(2, 3, figsize=(40, 20))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+    while True:
+        fig = sns.heatmap(np.flipud(policy), cmap="YlGnBu", ax=axes[iterations])
+        fig.set_ylabel('# cars at first location', fontsize=30)
+        fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+        fig.set_xlabel('# cars at second location', fontsize=30)
+        fig.set_title('policy {}'.format(iterations), fontsize=30)
+
+        # policy evaluation (in-place)
+        while True:
+            old_value = value.copy()
+            for i in range(MAX_CARS + 1):
+                for j in range(MAX_CARS + 1):
+                    new_state_value = expected_return([i, j], policy[i, j], value, constant_returned_cars)
+                    value[i, j] = new_state_value
+            max_value_change = abs(old_value - value).max()
+            print('max value change {}'.format(max_value_change))
+            if max_value_change < 1e-4:
+                break
+
+        # policy improvement
+        policy_stable = True
+        for i in range(MAX_CARS + 1):
+            for j in range(MAX_CARS + 1):
+                old_action = policy[i, j]
+                action_returns = []
+                for action in actions:
+                    if (0 <= action <= i) or (-j <= action <= 0):
+                        action_returns.append(expected_return([i, j], action, value, constant_returned_cars))
+                    else:
+                        action_returns.append(-np.inf)
+                new_action = actions[np.argmax(action_returns)]
+                policy[i, j] = new_action
+                if policy_stable and old_action != new_action:
+                    policy_stable = False
+        print('policy stable {}'.format(policy_stable))
+
+        if policy_stable:
+            fig = sns.heatmap(np.flipud(value), cmap="YlGnBu", ax=axes[-1])
+            fig.set_ylabel('# cars at first location', fontsize=30)
+            fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+            fig.set_xlabel('# cars at second location', fontsize=30)
+            fig.set_title('optimal value', fontsize=30)
+            break
+
+        iterations += 1
+
+    plt.savefig(current_path + '/images/figure_4_2_1.png')
+    plt.close()
+
+def figure_4_2_2(constant_returned_cars=False):
+    value = np.zeros((MAX_CARS + 1, MAX_CARS + 1))
+    policy = np.zeros(value.shape, dtype=np.int)
+
+    iterations = 0
+    _, axes = plt.subplots(2, 3, figsize=(40, 20))
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    axes = axes.flatten()
+    while True:
+        fig = sns.heatmap(np.flipud(policy), cmap="YlGnBu", ax=axes[iterations])
+        fig.set_ylabel('# cars at first location', fontsize=30)
+        fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+        fig.set_xlabel('# cars at second location', fontsize=30)
+        fig.set_title('policy {}'.format(iterations), fontsize=30)
+
+        # policy evaluation (in-place)
+        while True:
+            old_value = value.copy()
+            for i in range(MAX_CARS + 1):
+                for j in range(MAX_CARS + 1):
+                    new_state_value = expected_return([i, j], policy[i, j], value, constant_returned_cars)
+                    value[i, j] = new_state_value
+            max_value_change = abs(old_value - value).max()
+            print('max value change {}'.format(max_value_change))
+            if max_value_change < 1e-4:
+                break
+
+        # policy improvement
+        policy_stable = True
+        for i in range(MAX_CARS + 1):
+            for j in range(MAX_CARS + 1):
+                old_action = policy[i, j]
+                action_returns = []
+                for action in actions:
+                    if (0 <= action <= i) or (-j <= action <= 0):
+                        action_returns.append(expected_return([i, j], action, value, constant_returned_cars))
+                    else:
+                        action_returns.append(-np.inf)
+                new_action = actions[np.argmax(action_returns)]
+                policy[i, j] = new_action
+                if policy_stable and old_action != new_action:
+                    policy_stable = False
+        print('policy stable {}'.format(policy_stable))
+
+        if policy_stable:
+            fig = sns.heatmap(np.flipud(value), cmap="YlGnBu", ax=axes[-1])
+            fig.set_ylabel('# cars at first location', fontsize=30)
+            fig.set_yticks(list(reversed(range(MAX_CARS + 1))))
+            fig.set_xlabel('# cars at second location', fontsize=30)
+            fig.set_title('optimal value', fontsize=30)
+            break
+
+        iterations += 1
+
+    plt.savefig(current_path + '/images/figure_4_2_2.png')
+    plt.close()
+
+
+if __name__ == '__main__':
+    figure_4_2_1()
+    #figure_4_2_2()
