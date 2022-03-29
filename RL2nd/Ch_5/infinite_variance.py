134454fe072d6e81e3707807f90b6f3eaf75c91d
sive2045 <lc9902130509@gmail.com>
2022-03-29 20:52:14 +0900

    feat: Ex 5.5 Ordinary Importance Sampling and Weighted Importance Sampling p. 106~108

--- RL2nd/Ch_5/infinite_variance.py
+++ RL2nd/Ch_5/infinite_variance.py
@@ -5,6 +5,11 @@ import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 matplotlib.use('Agg')
 
+"""
+Ordinary importance-sampling
+-> weighted importance-sampling도 해보기.
+"""
+
 ACTION_BACK = 0
 ACTION_END = 1
 
@@ -30,7 +35,7 @@ def play():
 
 def figure_5_4():
     runs = 10
-    episodes = 100000
+    episodes = 100_000
     for run in range(runs):
         rewards = []
         for episode in range(0, episodes):
@@ -50,5 +55,36 @@ def figure_5_4():
     plt.savefig(current_path + '/images/figure_5_4.png')
     plt.close()
 
+def figure_5_4_1():
+    """
+    Using Weighted Importance Sampling
+    -> Results (NaN, 1, ,,,): p. 107 참조 
+    """
+    runs = 10
+    episodes = 100_000
+    for run in range(runs):
+        rewards = []
+        rhos = []
+        for episode in range(0, episodes):
+            reward, trajectory = play()
+            if trajectory[-1] == ACTION_END:
+                rho = 0
+            else:
+                rho = 1.0 / pow(0.5, len(trajectory))
+            rewards.append(rho * reward)
+            rhos.append(rho)
+        rewards = np.add.accumulate(rewards)
+        rhos = np.add.accumulate(rhos)
+        estimations = np.asarray(rewards) / np.asarray(rhos)
+        print(estimations)
+        plt.plot(estimations)
+    plt.xlabel('Episodes (log scale)')
+    plt.ylabel('Weighted Importance Sampling')
+    plt.xscale('log')
+
+    plt.savefig(current_path + '/images/figure_5_4_1.png')
+    plt.close()
+
 if __name__ == '__main__':
     figure_5_4()
+    figure_5_4_1()
