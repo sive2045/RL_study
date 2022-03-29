3175977c2823f2d43ccba8975df8281a399cb288
sive2045 <lc9902130509@gmail.com>
2022-03-29 22:00:48 +0900

    init: racetrack

---
+++ RL2nd/Ch_5/racetrack.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+import os
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+"""
+Example 5.12 Racetrack
+"""
+
