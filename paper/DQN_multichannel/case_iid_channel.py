9995d155a7029399138cb6af20ea4301d46e4d73
sive2045 <lc9902130509@gmail.com>
2022-05-19 18:30:18 +0900

    feat: multiple channel

--- paper/DQN_multichannel/case_iid_channel.py
+++ paper/DQN_multichannel/case_iid_channel.py
@@ -12,8 +12,21 @@ from tensorflow.keras.optimizers import Adam
 from keras.models import Sequential
 
 EPISODES = 500_000
-CHANNEL = 100 * np.ones(16)
-threshold_prob = 0.5
+CHANNEL = 10 * np.ones(16) # init channel
 
-def iid_channel(channels ,transition_prob):
-    pass
+def make_multiple_good_channel_model(switching_prob=0.9, channel_size=16, time_slot=50):
+    """
+    channel_size 변화시 코드 수정 필요
+    """
+    channels = np.zeros((time_slot, channel_size))
+    fixed_channel_order = np.array([[8, 9, 11, 13],[2, 7, 10, 12], [0, 1, 4, 5], [3, 6, 14, 15]])
+
+    idx_order = 0
+    for time in range(time_slot):
+        channels[time][fixed_channel_order[idx_order]] = 1
+
+        if np.random.binomial(1, switching_prob) == 1:
+            idx_order = idx_order + 1 if idx_order < 3 else 0
+    
+    print(f'Channel Set: \n{channels}')
+    return channels
